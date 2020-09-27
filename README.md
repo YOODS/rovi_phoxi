58500c2418a5dd276764f5fcec99ea3df963d8d1
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2020-09-27 12:35:27 +0900

    Added fdriver

--- README.md
+++ README.md
@@ -1,22 +1,37 @@
 # rovi_phoxi
-RoVI wrapper for PhoXi
+RoVI wrappers for PhoXi
 
-## Topics
-### To publish
+## fdriver.py  
+PhoXiControlの出力ファイルをroviに再発行します。ファイルは**.ros/phoxi**以下にあるとみなします。
+
+### To launch
+~~~
+roslaunch rovi_phoxi fdriver.launch
+~~~
+
+### Topics to publish
 <table>
 <tr><th>Name<th>Type<th>Description
-<tr><td>ps_floats_all<td>Numpy<td>3DデータNumpy形式。全ピクセルの三次元座標をアドレス順に出力します。
+<tr><td>ps_floats_all<td>Numpy<td>3DデータNumpy形式。全ピクセルの三次元座標をピクセル順に出力します。
 <tr><td>ps_floats<td>Numpy<td>3DデータNumpy形式。三次元座標が求められた点のみ出力します。
-<tr><td>ps_pc<td>PointCloud<td>3DデータPointCloud形式。三次元座標が求められた点のみ出力します。
-<tr><td>left/image_raw<td>Image<td>左カメラraw画像
-<tr><td>left/image_rect<td>Image<td>左カメラrectify画像
-<tr><td>/error<td>String<td>
-<tr><td>stat<td>Bool<td>接続状態
-<tr><td>Y1<td>Bool<td>撮像結果(X1に対するレスポンス)
+<tr><td>image_raw<td>Image<td>最新のraw画像を１秒毎に更新します
+<tr><td>image_raw1<td>Image<td>点群取得時にそのraw画像を出力します
+<tr><td>Y1<td>Bool<td>X1処理完了
 </table>
 
-### To subscribe
+### Topics to subscribe
 <table>
 <tr><th>Name<th>Type<th>Description
-<tr><td>X1<td>Bool<td>撮像トリガ
+<tr><td>X1<td>Bool<td>PLYファイルをps_floatに出力します
 </table>
+
+## ユーティリティ
+### dump_caminfo  
+PhoXiカメラパラメータ(K,D)をyaml形式で出力します。以下手順にてphoxiのカメラパラメータファイルを用意します。
+~~~
+?????
+~~~
+### ロボットキャリブレーション
+~~~
+roslaunch rovi_phoxi rcalib_wpc.launch
+~~~
---
+++ script/fdriver.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import numpy as np
+import cv2
+import glob
+import sys
+
+import roslib
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+Config={
+  "img":"/home/ca/.ros/phoxi/IMG*.jpg"
+}
+
+ImgFile=''
+ImgDat=None
+
+def cb_fchk(msg):
+  global ImgFile,ImgDat
+  dl=glob.glob(Config["img"])
+  dl.sort(reverse=True)
+  if ImgFile!=dl[0]:
+    im=cv2.imread(dl[0])
+    im=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
+    ImgDat=bridge.cv2_to_imgmsg(im,'mono8')
+    ImgFile=dl[0]
+  if ImgDat is not None: impub.publish(ImgDat)
+  rospy.Timer(rospy.Duration(1),cb_fchk,oneshot=True)
+  return
+
+def parse_argv(argv):
+  args={}
+  for arg in argv:
+    tokens = arg.split(":=")
+    if len(tokens) == 2:
+      key = tokens[0]
+      args[key]=tokens[1]
+  return args
+
+########################################################
+rospy.init_node("fdriver",anonymous=True)
+Config.update(parse_argv(sys.argv))
+try:
+  Config.update(rospy.get_param("~config"))
+except Exception as e:
+  print "get_param exception:",e.args
+print "Config",Config
+
+bridge=CvBridge()
+impub=rospy.Publisher('~image_rect',Image,queue_size=1)
+
+rospy.Timer(rospy.Duration(1),cb_fchk,oneshot=True) #to check change of file
+try:
+  rospy.spin()
+except KeyboardInterrupt:
+  print "Shutting down"
