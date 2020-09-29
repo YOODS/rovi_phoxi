fb08ed7e1d0d605cc66208742cbf677d8085e758
KazukiHirazumi <ca-giken@c-able.ne.jp>
2020-09-29 18:50:19 +0900

    rcalibテスト中

--- script/fdriver.py
+++ script/fdriver.py
@@ -11,7 +11,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 Config={
-  "img":"/home/ca/.ros/phoxi/IMG*.jpg"
+  "img":"../.PhotoneoPhoXiControl/Output/scan*.png"
 }
 
 ImgFile=''
@@ -20,13 +20,15 @@ ImgDat=None
 def cb_fchk(msg):
   global ImgFile,ImgDat
   dl=glob.glob(Config["img"])
-  dl.sort(reverse=True)
-  if ImgFile!=dl[0]:
-    im=cv2.imread(dl[0])
-    im=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
-    ImgDat=bridge.cv2_to_imgmsg(im,'mono8')
-    ImgFile=dl[0]
-  if ImgDat is not None: impub.publish(ImgDat)
+  if len(dl)>0:
+    dl.sort(reverse=True)
+    if ImgFile!=dl[0]:
+      im=cv2.imread(dl[0])
+      print "imread",dl[0]
+      im=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
+      ImgDat=bridge.cv2_to_imgmsg(im,'mono8')
+      ImgFile=dl[0]
+    if ImgDat is not None: impub.publish(ImgDat)
   rospy.Timer(rospy.Duration(1),cb_fchk,oneshot=True)
   return
 
@@ -49,7 +51,7 @@ except Exception as e:
 print "Config",Config
 
 bridge=CvBridge()
-impub=rospy.Publisher('~image_rect',Image,queue_size=1)
+impub=rospy.Publisher('~image_raw',Image,queue_size=1)
 
 rospy.Timer(rospy.Duration(1),cb_fchk,oneshot=True) #to check change of file
 try:
